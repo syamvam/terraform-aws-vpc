1dd0274762db3e740f4a7d0528a97381ebf7c05a
syamkumary <syamyala7@gmail.com>
2026-06-25 12:56:13 +0530

    dbgroup subnet output value exposed

--- outputs.tf
+++ outputs.tf
@@ -17,6 +17,10 @@ output "database_subnet_ids" {
   value = aws_subnet.database[*].id
 }
 
+output "database_subnet_group_ids" {
+  value = aws_db_subnet_group.default.name
+}
+
 # output "default_vpc_info" {
 #   value = data.aws_vpc.default
 # }
